d8da5bf2c46bb89bb92a1c80f24ec950d1f94125
iamchrisas <chrisas.agb@gmail.com>
2024-03-16 17:34:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/", indexRoutes);
+app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
